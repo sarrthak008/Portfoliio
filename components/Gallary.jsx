21d691674c1c61446b68120097413ce7dfb51938
sarrthak008 <sarthaknavale08@gmail.com>
2025-05-25 14:58:03 +0300

    add close window function

--- components/Gallary.jsx
+++ components/Gallary.jsx
@@ -5,37 +5,41 @@ import styles from '@/styles/main.module.css'
 import Image from 'next/image'
 import GALLARY from "@/assets/gallary.png"
 import Shdows from './Shdows'
-import { openAnimation } from '@/lib/animationFunc'
+import { openAnimation, closeAnimation } from '@/lib/animationFunc'
 
-const GallaryOpener =({setOpenGallary})=>{
+const GallaryOpener = ({ setOpenGallary }) => {
 
-    openAnimation("box") // import open animation from the lib 
-    
+  openAnimation("box") // import open animation from the lib 
 
 
-     return(
-         <div className={`box h-0 w-0 absolute top-[50%] left-[50%] overflow-hidden translate-x-[-50%] translate-y-[-50%] ${styles.glass}`}>
-              <div className='w-72 h-60 absolute bottom-0 right-0'>
-                 <Shdows color1='red' color2='purple' intencity={100} />
-              </div>
-             <div className='float-right cursor-pointer text-3xl bg-rose-400' onClick={()=>setOpenGallary(false)}><i className="ri-close-fill"></i></div>
-         </div>
-     )
+  const handelClose=()=>{
+    closeAnimation("box",()=>setOpenGallary(false))
+  }
+
+
+  return (
+    <div className={`box h-0 w-0 absolute top-[50%] left-[50%] overflow-hidden translate-x-[-50%] translate-y-[-50%] ${styles.glass}`}>
+      <div className='w-72 h-60 absolute bottom-0 right-0'>
+        <Shdows color1='red' color2='purple' intencity={100} />
+      </div>
+      <div className='float-right cursor-pointer text-3xl bg-rose-400' onClick={()=>handelClose()}><i className="ri-close-fill"></i></div>
+    </div>
+  )
 }
 
 
 
 const Gallary = () => {
 
-    const [openGallary, setOpenGallary] = useState(false)
+  const [openGallary, setOpenGallary] = useState(false)
 
   return (
     <>
-      <div className={`${styles.glass} h-[50px] w-[50px] cursor-pointer`} onClick={()=>setOpenGallary(true)}>
-         <Image height="100%" width="100%" src={GALLARY} alt='gallary'/>
-    </div>
+      <div className={`${styles.glass} h-[50px] w-[50px] cursor-pointer`} onClick={() => setOpenGallary(true)}>
+        <Image height="100%" width="100%" src={GALLARY} alt='gallary' />
+      </div>
 
-     {openGallary && <GallaryOpener setOpenGallary={setOpenGallary}/>}
+      {openGallary && <GallaryOpener setOpenGallary={setOpenGallary} />}
     </>
   )
 }
